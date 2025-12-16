324bb76dd2e3e03e7aece9859d9eaeee168c95ad
Angelsospedra <angelsospedramartinez@gmail.com>
2025-12-16 12:24:19 +0100

    preparacion catalogo.php

--- catalogo.php
+++ catalogo.php
@@ -2,4 +2,10 @@
     require "conexion.php";
 
     
+    //tenemos que hacer un catalogo en base a lo que hay en la base de datos 
+    //para que los clientes puedan ver el catalogo
+    //seria ideal dividirse la pagina en branches
+
+    //os habla el angel y salva del año pasado <3
+
     ?>    
