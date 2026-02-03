2c5580d2c2b7221b6fe0b399d03ef0e28f4f9dba
Angelsospedra <angelsospedramartinez@gmail.com>
2026-02-03 09:34:45 +0100

    Update conexion.php

--- backend/conexion.php
+++ backend/conexion.php
@@ -1,12 +1,9 @@
 <?php
 
-// Cargar configuración
-$config = require 'config.php';
-
-$host = $config['host'];
-$user = $config['user'];
-$pass = $config['pass'];
-$db = $config['db'];
+$host=getenv('MYSQLHOST');
+$user=getenv('MYSQLUSER');
+$pass=getenv('MYSQLPASSWORD');
+$db=getenv('MYSQLDATABASE');
 
 //Conexión
 $conn = new mysqli($host, $user, $pass, $db);
